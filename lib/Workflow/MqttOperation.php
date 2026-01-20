b9c6419a1feb8e6b17dcef538bfb43887548d8cf
mamirov <m.amirov@gmail.com>
2026-01-20 22:46:43 +0500

    add json serilization, add talk events

--- lib/Workflow/MqttOperation.php
+++ lib/Workflow/MqttOperation.php
@@ -2,12 +2,19 @@
 
 namespace OCA\TalkMqtt\Workflow;
 
+use OCA\Talk\Events\BeforeAttendeesAddedEvent;
+use OCA\Talk\Events\BeforeCallEndedEvent;
 use OCA\Talk\Events\BeforeCallStartedEvent;
+use OCA\Talk\Events\BeforeUserJoinedRoomEvent;
+use OCA\Talk\Events\LobbyModifiedEvent;
+use OCA\Talk\Events\RoomModifiedEvent;
+use OCA\Talk\Events\SessionLeftRoomEvent;
 use OCA\TalkMqtt\Service\MqttService;
 use OCP\EventDispatcher\Event;
 use OCP\WorkflowEngine\IManager;
 use OCP\WorkflowEngine\IRuleMatcher;
 use OCP\WorkflowEngine\ISpecificOperation;
+use ReflectionClass;
 
 class MqttOperation implements ISpecificOperation
 {
@@ -48,20 +55,54 @@ class MqttOperation implements ISpecificOperation
 
     function onEvent(string $eventName, Event $event, IRuleMatcher $ruleMatcher): void
     {
-        if (!$event instanceof BeforeCallStartedEvent) {
+        if (!$event instanceof BeforeCallStartedEvent 
+        && !$event instanceof BeforeCallEndedEvent 
+        && !$event instanceof BeforeUserJoinedRoomEvent
+        && !$event instanceof BeforeAttendeesAddedEvent
+        && !$event instanceof LobbyModifiedEvent
+        && !$event instanceof SessionLeftRoomEvent
+        && !$event instanceof RoomModifiedEvent) {
             return;
         }
-        $room = $event->getRoom();
-        $payload = json_encode([
-            'time'    => time(),
-            'event' => 'BeforeCallStartedEvent',
-            'actor' => $event->getActor()->getAttendee()->getDisplayName(),
-            'room' => [
-                'name' => $room->getName(),
-                'token' => $room->getToken(),
-            ],
-            'link' => $event->getRoom()->getRemoteServer()
-        ]);
+        $payload = [];
+        $payload['event'] = $event::class;
+        $payload['time'] = time();
+        $payload = $this->serializeJson($event, $payload);
+        $payload = json_encode($payload);
         $this->mqtt->sendEvent("nextcloud/talk/messages", $payload);
+    }
+
+    private function serializeJson($class, $payload)
+    {
+        if (is_array($class)) {
+            foreach ($class as $arrayEl) {
+                $payload[] = $this->serializeJson($arrayEl, $payload);
+            }
+        }
+        $methods = get_class_methods($class);
+        $reflectionClass = new ReflectionClass($class::class);
+        foreach ($methods as $method) {
+            if ($method == 'getFieldTypes') {
+                foreach ($class->getFieldTypes() as $fieldType => $value) {
+                    $payload[$fieldType] = $class->{'get' . ucfirst($fieldType)}();
+                }
+                continue;
+            }
+            if (str_starts_with($method, 'get') || str_starts_with($method, 'is')) {
+                $fieldName = str_starts_with($method, 'get') ? lcfirst(substr($method, 3)) : $method;
+                $refMethod = $reflectionClass->getMethod($method);
+                if ($refMethod->getNumberOfParameters() > 0) {
+                    continue;
+                }
+                $fieldValue = $class->$method();
+                if (is_object($fieldValue) || (is_array($fieldValue) && !empty($fieldValue) && is_object($fieldValue[0]))) {
+                    $payload[$fieldName] = [];
+                    $payload[$fieldName] = $this->serializeJson($fieldValue, $payload[$fieldName]);
+                } else {
+                    $payload[$fieldName] = $fieldValue;
+                }
+            }
+        }
+        return $payload;
     }
 }
--- lib/Workflow/TalkCallEntity.php
+++ lib/Workflow/TalkCallEntity.php
@@ -2,13 +2,22 @@
 
 namespace OCA\TalkMqtt\Workflow;
 
+use OCA\Talk\Events\BeforeAttendeesAddedEvent;
+use OCA\Talk\Events\BeforeCallEndedEvent;
 use OCA\Talk\Events\BeforeCallStartedEvent;
+use OCA\Talk\Events\BeforeUserJoinedRoomEvent;
+use OCA\Talk\Events\LobbyModifiedEvent;
+use OCA\Talk\Events\RoomModifiedEvent;
+use OCA\Talk\Events\SessionLeftRoomEvent;
 use OCP\EventDispatcher\Event;
 use OCP\WorkflowEngine\GenericEntityEvent;
 use OCP\WorkflowEngine\IEntity;
 use OCP\WorkflowEngine\IRuleMatcher;
 
-class TalkCallEntity implements IEntity {
+use function OCP\Log\logger;
+
+class TalkCallEntity implements IEntity
+{
     function getName(): string
     {
         return "Nextcloud Talk call events";
@@ -18,19 +27,32 @@ class TalkCallEntity implements IEntity {
     {
         return "";
     }
-    
+
     function getEvents(): array
     {
         return [
-            new GenericEntityEvent("Conv started", "OCA\Talk\Events\BeforeCallStartedEvent"),
+            new GenericEntityEvent("Call started", "OCA\Talk\Events\BeforeCallStartedEvent"),
+            new GenericEntityEvent("Call ended", "OCA\Talk\Events\BeforeCallEndedEvent"),
+            new GenericEntityEvent("User joined room", "OCA\Talk\Events\BeforeUserJoinedRoomEvent"),
+            new GenericEntityEvent("Attendees added", "OCA\Talk\Events\BeforeAttendeesAddedEvent"),
+            new GenericEntityEvent("Lobby modified", "OCA\Talk\Events\LobbyModifiedEvent"),
+            new GenericEntityEvent("Session left room", "OCA\Talk\Events\SessionLeftRoomEvent"),
+            new GenericEntityEvent("Room modified", "OCA\Talk\Events\RoomModifiedEvent"),
         ];
     }
 
     function prepareRuleMatcher(IRuleMatcher $ruleMatcher, string $eventName, Event $event): void
     {
-        if (!$event instanceof BeforeCallStartedEvent) {
+        if (!$event instanceof BeforeCallStartedEvent 
+        && !$event instanceof BeforeCallEndedEvent 
+        && !$event instanceof BeforeUserJoinedRoomEvent
+        && !$event instanceof BeforeAttendeesAddedEvent
+        && !$event instanceof LobbyModifiedEvent
+        && !$event instanceof SessionLeftRoomEvent
+        && !$event instanceof RoomModifiedEvent) {
             return;
         }
+        logger('talkmqtt')->error("Preparing rule matcher for event: " . $eventName);
     }
 
     function isLegitimatedForUserId(string $userId): bool
