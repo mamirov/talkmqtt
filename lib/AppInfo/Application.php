3c5fe416787041c450cdca6448e2b8f1d4c04091
mamirov <m.amirov@gmail.com>
2026-01-28 23:08:09 +0500

    change to event listner

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -8,11 +8,8 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\EventDispatcher\IEventDispatcher;
 use OCA\Talk\Events\BeforeCallStartedEvent;
 use OCA\TalkMqtt\Listener\TalkMessageListener;
-use OCA\TalkMqtt\Service\MqttService;
-use Psr\Container\ContainerInterface;
 
 class Application extends App implements IBootstrap
 {
@@ -22,15 +19,13 @@ class Application extends App implements IBootstrap
 	public function __construct()
 	{
 		parent::__construct(self::APP_ID);
-		$dispatcher = $this->getContainer()->get(IEventDispatcher::class);
-		$dispatcher->addServiceListener(
-			BeforeCallStartedEvent::class,
-			TalkMessageListener::class,
-		);
 	}
 
-	public function register(IRegistrationContext $context): void {
+	public function register(IRegistrationContext $context): void
+	{
 		include_once __DIR__ . '/../../vendor/autoload.php';
+
+		$context->registerEventListener(BeforeCallStartedEvent::class, TalkMessageListener::class);
 	}
 
 	public function boot(IBootContext $context): void {}
